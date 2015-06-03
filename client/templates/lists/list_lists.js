bdcfc312a423a8267a76ab12b55760445fda76d5
Emmanuel Leveque <eleveque@hi-media.com>
2015-06-03 11:37:49 +0200

    Ajout de listes

--- client/templates/lists/list_lists.js
+++ client/templates/lists/list_lists.js
@@ -3,3 +3,27 @@ Template.listsList.helpers({
         return Lists.find();
     }
 });
+
+Template.listsList.events({
+    "click #add-list": function(event) {
+        $('.popup#add-new-list').show();
+    },
+    "submit .new-list": function(event) {
+        var name = event.target.name.value;
+        var color = event.target.color.value;
+        
+        console.log('plop');
+
+        Lists.insert({
+            name: name,
+            color: color,
+            createdAt: new Date()
+        })
+
+        event.target.name.value = "";
+        event.target.color.value = "";
+
+        $('.popup#add-new-list').hide();
+        return false;
+    }
+});
